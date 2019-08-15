41ecbb781de26126a940658cd66f66dbcca92f82
Ulrik Sandberg <UJSandberg@gmail.com>
2019-08-15 15:30:45 +0200

    Added ssh fingerprints

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,4 +1,3 @@
-ssh -i $TEST_KEY ubuntu@34.77.97.162
-docker run -p 5000:5000 ulriksandberg/ca-project:$CIRCLE_SHA1
-
+ssh ubuntu@34.77.97.162 "docker run -p 5000:5000 ulriksandberg/ca-project:$CIRCLE_SHA1 -d"
+exit
 
