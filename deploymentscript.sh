7629c7161fcdeac2ff066639f6f7384aac477058
Ulrik Sandberg <UJSandberg@gmail.com>
2019-08-15 15:37:17 +0200

    Fixed script

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,3 +1,3 @@
-ssh ubuntu@34.77.97.162 "docker run -p 5000:5000 ulriksandberg/ca-project:$CIRCLE_SHA1 -d"
+ssh ubuntu@34.77.97.162 "docker run -d -p 5000:5000 ulriksandberg/ca-project:$CIRCLE_SHA1"
 exit
 
